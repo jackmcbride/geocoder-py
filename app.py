e6d86146ec5c53c3ed9d319f2578ee4247a98527
Jack McBride <jlgm2@kent.ac.uk>
2017-10-25 17:41:37 +0100

    Introduced file deletion

--- app.py
+++ app.py
@@ -43,30 +43,23 @@ def success():
 
 @app.route('/table')
 def table():
-    try:
-        os.remove("templates/table.html")
-    except OSError:
-        pass
-    generate_table("uploads/uploaded_data.csv")
+    
     return render_template("success.html", table="table_frame.html", 
                             btns="button_panel.html")
 
 @app.route('/map')
 def map():
-    try:
-        os.remove("templates/webmap.html")
-    except OSError:
-        pass
-    generate_webmap("uploads/uploaded_data.csv")
     return render_template("success.html", map="map_frame.html",
     btns="button_panel.html")
 
 @app.route('/view_map')
 def view_map():
+    generate_webmap("uploads/uploaded_data.csv")
     return render_template("webmap.html")
 
 @app.route('/view_table')
 def view_table():
+    generate_table("uploads/uploaded_data.csv")
     return render_template("table.html")
 
 @app.route("/download")
